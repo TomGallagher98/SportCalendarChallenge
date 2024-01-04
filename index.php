21a40765f4b84ca03d539e3482aa490d39e19994
TomGallagher98 <tom@dataperspective.com.au>
2024-01-04 18:15:47 +1100

    Frontend - created index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- <link rel="stylesheet" href="css/styles.css"> -->
+    <title>Calendar</title>
+</head>
+
+<body>
+    <header>
+        <h1> Sports Calendar </h1>
+        <p class="subtitle">
+            Choose between Calendar and List display
+        </p>
+    </header>
+
+    <main>
+        
+    </main>
+</body>
