092b9f869605719500ca03257d763c60a77fe254
TomGallagher98 <tom@dataperspective.com.au>
2024-01-05 16:37:48 +1100

    Add-event - added event entry

--- add_event.php
+++ add_event.php
@@ -1,37 +1,39 @@
+<?php
+require 'php_scripts/filter_sports.php';
+$sports = getSports();
+?>
+
 <div>
     <h1>
         Add new event
     </h1>
-         <form action="insert.php" method="post">
+         <form action="/php_scripts/add_event_db.php" method="post">
              
             <p>
                <label for="date">Date:</label>
-               <input type="text" name="date" id="date">
+               <input type="text" name="date" id="date" placeholder="yyyy-mm-dd">
             </p>
- 
              
             <p>
                <label for="time">Time:</label>
-               <input type="text" name="time" id="time">
+               <input type="text" name="time" id="time" placeholder="HH:MM:SS">
             </p>
  
-             
             <p>
                <label for="sport">Sport:</label>
-               <input type="text" name="sport" id="sport">
+               <input type="text" name="sport" id="sport", list=$sports>
             </p>
- 
-             
+            
             <p>
                <label for="home_team">Home Team:</label>
-               <input type="text" name="home_team" id="home_team">
+               <input type="text" name="home_team" id="home_team" >
             </p>
- 
-             
+            
             <p>
                <label for="away_team">Away Team:</label>
                <input type="text" name="away_team" id="away_team">
             </p>
+
             <p>
                <label for="venue">Venue:</label>
                <input type="text" name="venue" id="venue">
---
+++ php_scripts/add_event_db.php
@@ -0,0 +1,31 @@
+<?php
+// require "database_connection.php";
+
+if (!defined('SERVER')) define('SERVER', "localhost");
+    if (!defined('USERNAME')) define('USERNAME', "root");
+    if (!defined('PASSWORD')) define("PASSWORD", "6440");
+    if (!defined('DATABASE')) define("DATABASE", 'sportradarcalendarchallenge');
+    $mysqli = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
+    
+
+    $event_date =  $_REQUEST['date'];
+    $event_time = $_REQUEST['time'];
+    $sport =  $_REQUEST['sport'];
+    $home_team = $_REQUEST['home_team'];
+    $away_team = $_REQUEST['away_team'];
+    $stadium = $_REQUEST['venue'];
+    settype($home_team, "integer");
+    settype($away_team, "integer");
+    settype($stadium, "integer");
+    // $mysqli -> dbConnect();
+    $sql = "INSERT INTO calendar_entries VALUES
+    (0, '$sport', $home_team, $away_team, '$event_date', '$event_time', $stadium )";
+
+    if ($mysqli -> query($sql) == TRUE) {
+        echo "Successfully Added Event";
+    } else {
+        echo "Error:" . $sql . "<br>" . $mysqli->error;
+    }
+
+    $mysqli -> close();
+    ?>
